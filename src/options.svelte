6a24a2b26f038233ca981073d22cf850416a8b7a
Jakob Essbüchl <jakob@5fold.de>
2022-05-30 18:27:30 +0200

    Format options.svelte

--- src/options.svelte
+++ src/options.svelte
@@ -6,6 +6,10 @@
   let token = "";
   let resultNum = 10;
   let openLinkType = "newTab";
+  let theme = {
+    duckduckgo: "auto",
+    google: "auto",
+  };
   let isSuccess = false;
   let isError = false;
 
@@ -15,6 +19,7 @@
     token = config.token;
     resultNum = config.resultNum;
     openLinkType = config.openLinkType;
+    theme = config.theme;
   }
 
   init();
@@ -24,7 +29,8 @@
       baseUrl,
       token,
       resultNum,
-      openLinkType
+      openLinkType,
+      theme,
     };
 
     const testResult = await testConnection(config);
@@ -39,67 +45,119 @@
     }
   }
 </script>
+
 <h6>Configuration</h6>
-<div class="divider"></div>
-<p>This is a companion extension for the <a href="https://github.com/sissbruecker/linkding">linkding</a> bookmark
-  service. Before you can start using it you have to configure some basic settings, so that the extension can
-  communicate with your linkding installation.</p>
+<div class="divider" />
+<p>
+  This is a companion extension for the <a
+    href="https://github.com/sissbruecker/linkding">linkding</a
+  > bookmark service. Before you can start using it you have to configure some basic
+  settings, so that the extension can communicate with your linkding installation.
+</p>
 <form class="form" on:submit|preventDefault={handleSubmit}>
   <div class="form-group">
     <label class="form-label" for="input-base-url">Base URL</label>
-    <input class="form-input" type="text" id="input-base-url" placeholder="https://linkding.mydomain.com"
-           bind:value={baseUrl}>
-    <div class="form-input-hint">The base URL of your linkding installation, <b>without</b> the <samp>/bookmark</samp> path or a trailing slash</div>
+    <input
+      class="form-input"
+      type="text"
+      id="input-base-url"
+      placeholder="https://linkding.mydomain.com"
+      bind:value={baseUrl}
+    />
+    <div class="form-input-hint">
+      The base URL of your linkding installation, <b>without</b> the
+      <samp>/bookmark</samp> path or a trailing slash
+    </div>
   </div>
   <div class="form-group">
     <label class="form-label" for="input-token">API Authentication Token</label>
-    <input class="form-input" type="password" id="input-token" placeholder="Token" bind:value={token}>
-    <div class="form-input-hint">Used to authenticate against the linkding API. You can find this on your linkding
-      settings page.
+    <input
+      class="form-input"
+      type="password"
+      id="input-token"
+      placeholder="Token"
+      bind:value={token}
+    />
+    <div class="form-input-hint">
+      Used to authenticate against the linkding API. You can find this on your
+      linkding settings page.
     </div>
   </div>
   <div class="form-group">
-    <label class="form-label" for="input-search-num">Maximum number of search results</label>
-    <input class="form-input" type="number" id="input-search-num" placeholder="10" bind:value={resultNum}>
-    <div class="form-input-hint">The maximum number of search results. High numbers could lead to worse performance.
+    <label class="form-label" for="input-search-num"
+      >Maximum number of search results
+    </label>
+    <input
+      class="form-input"
+      type="number"
+      id="input-search-num"
+      placeholder="10"
+      bind:value={resultNum}
+    />
+    <div class="form-input-hint">
+      The maximum number of search results. High numbers could lead to worse
+      performance.
     </div>
   </div>
   <div class="form-group">
-    <label class="form-label" for="input-link-type">Default open link type</label>
-    <label class="form-radio">
-      <input type="radio" id="input-link-type" bind:group={openLinkType} value="newTab">
-      <i class="form-icon"></i>Open links in a new tab (default)
+    <label class="form-label" for="input-link-type"
+      >Default open link type
     </label>
     <label class="form-radio">
-      <input type="radio" id="input-link-type" bind:group={openLinkType} value="sameTab">
-      <i class="form-icon"></i>Open links in the same tab
+      <input
+        type="radio"
+        id="input-link-type"
+        bind:group={openLinkType}
+        value="newTab"
+      />
+      <i class="form-icon" />Open links in a new tab (default)
+    </label>
+    <label class="form-radio">
+      <input
+        type="radio"
+        id="input-link-type"
+        bind:group={openLinkType}
+        value="sameTab"
+      />
+      <i class="form-icon" />Open links in the same tab
     </label>
   </div>
-  <div class="divider"></div>
+
+  <div class="divider" />
+
   <div class="button-row">
     {#if isSuccess}
       <div class="form-group has-success mr-2">
-        <span class="form-input-hint"><i class="icon icon-check"></i> Connection successful</span>
+        <span class="form-input-hint"
+          ><i class="icon icon-check" /> Connection successful
+        </span>
       </div>
     {/if}
     {#if isError}
       <div class="form-group has-error mr-2">
-        <span class="form-input-hint"><i class="icon icon-cross"></i> Connection failed</span>
+        <span class="form-input-hint">
+          <i class="icon icon-cross" /> Connection failed
+        </span>
       </div>
     {/if}
-    <button type="submit" class="btn btn-primary ml-2" disabled={!(baseUrl && token)}>
+    <button
+      type="submit"
+      class="btn btn-primary ml-2"
+      disabled={!(baseUrl && token)}
+    >
       Save
     </button>
   </div>
 </form>
+
 <style>
-    .button-row {
-        display: flex;
-        justify-content: flex-end;
-        align-items: baseline;
-    }
-    .button-row button {
-        padding-left: 32px;
-        padding-right: 32px;
-    }
+  .button-row {
+    display: flex;
+    justify-content: flex-end;
+    align-items: baseline;
+  }
+  .button-row button {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
 </style>
