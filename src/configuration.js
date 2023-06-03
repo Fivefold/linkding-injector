9ccadc97248da47d22845a4b0da5864802558f1a
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-06-04 02:55:56 +0800

    Fix get config error & refactor

--- src/configuration.js
+++ src/configuration.js
@@ -2,31 +2,24 @@ import { getStorage } from "./browser";
 
 const CONFIG_KEY = "ld_ext_config";
 
-export async function getConfiguration() {
-  let config = "";
-
-  const configJson = await getStorage().get(CONFIG_KEY);
-
-  if (
-    // if there is no saved configuration, save a default configuration
-    configJson &&
-    Object.keys(configJson).length === 0 &&
-    Object.getPrototypeOf(configJson) === Object.prototype
-  ) {
-    config = {
-      baseUrl: "",
-      token: "",
-      resultNum: 10,
-      openLinkType: "newTab",
-      themeGoogle: "auto",
-      themeDuckduckgo: "auto",
-      themeBrave: "auto",
-      themeSearx: "auto",
-    };
-  } else {
-    config = JSON.parse(configJson[CONFIG_KEY]);
-  }
-  return config;
+export const getConfiguration = async () => {
+  return new Promise((resolve) => {
+    getStorage().get(CONFIG_KEY, (data) => {
+      const config = JSON.parse(
+        data[CONFIG_KEY] || {
+          baseUrl: '',
+          token: '',
+          resultNum: 10,
+          openLinkType: 'newTab',
+          themeGoogle: 'auto',
+          themeDuckduckgo: 'auto',
+          themeBrave: 'auto',
+          themeSearx: 'auto',
+        }
+      )
+      resolve(config)
+    })
+  })
 }
 
 export function saveConfiguration(config) {
@@ -34,12 +27,7 @@ export function saveConfiguration(config) {
   getStorage().set({ [CONFIG_KEY]: configJson });
 }
 
-export async function isConfigurationComplete() {
-  const config = await getConfiguration();
-
-  if (config.baseUrl === "" || config.token === "") {
-    return false;
-  } else {
-    return true
-  }
+export const isConfigurationComplete = async () => {
+  const { baseUrl, token } = await getConfiguration()
+  return !!baseUrl && !!token
 }
