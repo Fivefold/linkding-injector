de18c61e4150154749c3534c0a95c5e52293ed53
SevicheCC <91365763+Sevichecc@users.noreply.github.com>
2023-06-04 18:48:26 +0800

    Refactor configuration

--- src/configuration.js
+++ src/configuration.js
@@ -2,32 +2,32 @@ import { getStorage } from "./browser";
 
 const CONFIG_KEY = "ld_ext_config";
 
-export const getConfiguration = async () => {
+export async function getConfiguration() {
+  const DEFAULT_CONFIG = {
+    baseUrl: "",
+    token: "",
+    resultNum: 10,
+    openLinkType: "newTab",
+    themeGoogle: "auto",
+    themeDuckduckgo: "auto",
+    themeBrave: "auto",
+    themeSearx: "auto",
+  };
+
   return new Promise((resolve) => {
     getStorage().get(CONFIG_KEY, (data) => {
-      const config = JSON.parse(
-        data[CONFIG_KEY] || {
-          baseUrl: '',
-          token: '',
-          resultNum: 10,
-          openLinkType: 'newTab',
-          themeGoogle: 'auto',
-          themeDuckduckgo: 'auto',
-          themeBrave: 'auto',
-          themeSearx: 'auto',
-        }
-      )
-      resolve(config)
-    })
-  })
-}
+      const config = JSON.parse(data[CONFIG_KEY] || DEFAULT_CONFIG);
+      resolve(config);
+    });
+  });
+};
 
 export function saveConfiguration(config) {
   const configJson = JSON.stringify(config);
   getStorage().set({ [CONFIG_KEY]: configJson });
 }
 
-export const isConfigurationComplete = async () => {
-  const { baseUrl, token } = await getConfiguration()
-  return !!baseUrl && !!token
-}
+export async function isConfigurationComplete() {
+  const { baseUrl, token } = await getConfiguration();
+  return !!baseUrl && !!token;
+};
